382b707de9a639125f4d499227087f8e2629e103
neelamlonari <neelam.lonari@hotmail.com>
2022-11-28 21:16:05 -0500

    day1 odd even array

---
+++ day1.js
@@ -0,0 +1,11 @@
+let oddNum= "";
+let evenNums="";
+for( i=10;i<=100;i=i+20)
+{
+   
+evenNums=evenNums+" "+(i+10);
+oddNum= oddNum+" "+i;
+//console.log(i);
+}
+console.log(oddNum);
+console.log(evenNums);
